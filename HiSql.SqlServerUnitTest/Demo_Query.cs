781d2217048b937b587e747b83f9866ac252032b
guangmin Tang <tansar@126.com>
2022-03-09 08:48:03 +0800

    优化hisql like语句

--- HiSql.SqlServerUnitTest/Demo_Query.cs
+++ HiSql.SqlServerUnitTest/Demo_Query.cs
@@ -35,11 +35,18 @@ namespace HiSql.UnitTest
             //Query_Demo6(sqlClient);
             //Query_Demo7(sqlClient);
             //Query_Demo8(sqlClient);
-            Query_Demo9(sqlClient);
+            //Query_Demo9(sqlClient);
+            //Query_Demo10(sqlClient);
             var s = Console.ReadLine();
         }
 
-        
+
+
+        static void Query_Demo10(HiSqlClient sqlClient)
+        {
+            var _sql = sqlClient.HiSql("select * from Hi_FieldModel where TabName like 'H_D%'").ToSql();
+        }
+
 
         static void Query_Demo9(HiSqlClient sqlClient)
         {
