c9ad4dab4771201949dddf2801466e4991350f15
guangmin Tang <tansar@126.com>
2022-06-22 17:22:01 +0800

    1.0.4.2发布

--- HiSql.SqlServer/obj/Release/netcoreapp3.1/HiSql.SqlServer.AssemblyInfo.cs
+++ HiSql.SqlServer/obj/Release/netcoreapp3.1/HiSql.SqlServer.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiSql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("该包支持SqlServer数据库 依赖于HiSql.dll包运行\r\n")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.4.2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiSql.SqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiSql.SqlServer")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.4.2")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
