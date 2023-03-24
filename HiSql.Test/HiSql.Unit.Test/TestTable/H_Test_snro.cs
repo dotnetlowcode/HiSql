80fd2733b9cc4474e65aa5a300c7026326b8b2ed
tansar <tansar@126.com>
2023-03-24 16:42:30 +0800

    数据插入默认值
    解决Bool类型默认为true的问题

---
+++ HiSql.Test/HiSql.Unit.Test/TestTable/H_Test_snro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiSql.Unit.Test
+{
+    [System.Serializable]
+    [HiTable(IsEdit = true, TabName = "H_Test_snro")]
+    public class H_Test_snro:StandField
+    {
+        [HiColumn(FieldDesc = "工号", IsPrimary = true, IsBllKey = true, FieldType = HiType.VARCHAR,FieldLen =20,SNO = "WFNO",SNO_NUM ="1", SortNum = 1, IsSys = false, DBDefault = HiTypeDBDefault.EMPTY)]
+        public string UEID
+        {
+            get; set;
+        }
+        [HiColumn(FieldDesc = "姓名", FieldType = HiType.NVARCHAR, FieldLen = 50, IsNull = false, SortNum = 2, DBDefault = HiTypeDBDefault.EMPTY)]
+        public string UName
+        {
+            get; set;
+        }
+
+        [HiColumn(FieldDesc = "年龄", FieldType = HiType.INT, IsNull = false, SortNum = 3, IsSys = true, DBDefault = HiTypeDBDefault.EMPTY)]
+        public int Age
+        {
+            get; set;
+        }
+
+        [HiColumn(FieldDesc = "薪水", FieldType = HiType.DECIMAL, FieldDec = 2, FieldLen = 18, IsNull = false, SortNum = 4, DBDefault = HiTypeDBDefault.EMPTY)]
+        public int Salary
+        {
+            get; set;
+        }
+        [HiColumn(FieldDesc = "描述编号", FieldType = HiType.NVARCHAR, FieldLen = 100, IsNull = false, SortNum = 5, IsSys = true, DBDefault = HiTypeDBDefault.EMPTY)]
+        public string Descript
+        {
+            get; set;
+        }
+    }
+}
