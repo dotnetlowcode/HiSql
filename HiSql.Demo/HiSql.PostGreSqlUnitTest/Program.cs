d14e17527259b68a72375b09c02ca88abc1be81a
pengxianyuan <pengxianyuan517@126.com>
2025-01-06 11:18:09 +0800

    修复嵌套加锁的bug;

--- HiSql.Demo/HiSql.PostGreSqlUnitTest/Program.cs
+++ HiSql.Demo/HiSql.PostGreSqlUnitTest/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HiSql.PostGreSqlUnitTest
 {
@@ -9,6 +11,40 @@ namespace HiSql.PostGreSqlUnitTest
     {
         static void Main(string[] args)
         {
+
+            var LckInfo = new LckInfo() { Key = "test" };
+
+            var LckInfo2 = new LckInfo() { Key = "test" };
+
+            var rlt1 =  Lock.LockOnExecute("test", async  () => {
+                await Task.Run(() =>
+                {
+                    var rlt2 = Lock.LockOnExecute("test2", async () =>
+                    {
+
+                       await Task.Run(() =>
+                        {
+                            for (global::System.Int32 i = 0; i < 2; i++)
+                            {
+                                Console.WriteLine(" == " + i);
+
+                                Thread.Sleep(500);
+                            }
+                        });
+
+                    }, LckInfo);
+
+                    Thread.Sleep(500);
+                    Console.WriteLine(rlt2.ToString());
+
+                });
+
+            }, LckInfo);
+
+
+            Console.WriteLine(rlt1.ToString());
+
+
             var refreshtoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcmltYXJ5c2lkIjoiVTAwMDAxMDAwMSIsInVuaXF1ZV9uYW1lIjoiaGFkbWluIiwiVGhpcmRVc2VySUQiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3VzZXJkYXRhIjoie1wiVXNlcklkXCI6XCJVMDAwMDEwMDAxXCIsXCJEZXB0SWRcIjpcIjFcIixcIlVzZXJUeXBlXCI6OSxcIlVzZXJOYW1lXCI6XCJoYWRtaW5cIixcIk5pY2tOYW1lXCI6XCJcIixcIkxvZ2luSVBcIjpcIjEyNy4wLjAuMVwiLFwiVGhpcmRVc2VySURcIjpudWxsLFwiSXNBZG1pblwiOnRydWV9IiwibmJmIjoxNzM1ODg0OTA1LCJleHAiOjE3MzYzMTY5MDUsImlhdCI6MTczNTg4NDkwNX0.X1bzV_9BZyBp4Ac6LBSFXCAuK2ulykfI5FGvLoW7o7M";
 
             HiSql.Global.RedisOn = true;
